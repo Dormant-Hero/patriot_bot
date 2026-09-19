0c39b1fb1072b2dffc51e515a2dd45c046dd8305
Marko <mgo3kakashi@gmail.com>
2026-09-19 19:58:57 +0100

    configured to now use live non-test env variables

--- config.py
+++ config.py
@@ -12,11 +12,11 @@ def _required(key: str) -> str:
 
 OWNER_ID = int(_required("OWNER_ID"))
 # testing enviornment_variables
-DB_NAME = _required("TEST_DBNAME")
+DB_NAME = _required("DBNAME")
 PATRIOTS_ROLE_ID = int(_required("PATRIOT_ROLE_ID"))
 DB_USER = _required("USERNME")
 DB_PASSWORD = _required("PASSWORD")
 DB_HOST = _required("HOST")
 DB_PORT = _required("PORT")
 DB_CONNECTION_STRING = f"dbname={DB_NAME} user={DB_USER} password={DB_PASSWORD} host={DB_HOST} port={DB_PORT}"
-TOKEN = _required("TEST_TOKEN")
+TOKEN = _required("TOKEN")
